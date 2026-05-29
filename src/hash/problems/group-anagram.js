217d01b2156c36ecad17227cd49208671fc18ca1
Farzan <farzan@OFD001M6014.local>
2026-05-29 20:20:55 +0330

    add groupAnagrams

---
+++ src/hash/problems/group-anagram.js
@@ -0,0 +1,44 @@
+// Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+function groupAnagramsWithSorting(strs) {}
+
+console.log(
+  groupAnagramsWithSorting(["act", "pots", "tops", "cat", "stop", "hat"]),
+); // [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+console.log(groupAnagramsWithSorting(["x"])); // [["x"]]
+console.log(groupAnagramsWithSorting([""])); // [[""]]
+
+function groupAnagramsWithHashMap(strs) {
+  const result = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    const count = new Array(26).fill(0);
+    const str = strs[i];
+    // console.log("str", str);
+
+    for (let j = 0; j < str.length; j++) {
+      const char = str[j];
+      //   console.log("char", char);
+      console.log("char.charCodeAt(0)", char.charCodeAt(0));
+
+      count[char.charCodeAt(0) - "a".charCodeAt(0)] += 1;
+    }
+    // console.log(count);
+
+    const key = count.join(",");
+    // console.log("key", key);
+    if (!result[key]) {
+      result[key] = [];
+    }
+    result[key].push(str);
+    // console.log("result", result);
+  }
+
+  return Object.values(result);
+}
+
+console.log(
+  groupAnagramsWithHashMap(["act", "pots", "tops", "cat", "stop", "hat"]),
+); // [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+console.log(groupAnagramsWithHashMap(["x"])); // [["x"]]
+console.log(groupAnagramsWithHashMap([""])); // [[""]]
